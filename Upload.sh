89180c5de829a57831f620d6c51e249dd5f1a2b1
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-04-14 22:26:20 +0530

    Create Upload.sh

---
+++ Upload.sh
@@ -0,0 +1,7 @@
+echo "This will zip and upload"
+  curl -sL https://git.io/file-transfer | sh
+  sudo chmod -R 777 MountedImage
+  touch links.txt
+  echo "MountedImage link : " >> links.txt
+  gzip -v MountedImage/system.img
+  ./transfer trs MountedImage/system.img.gz | grep Download >> links.txt 
